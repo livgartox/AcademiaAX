90cfd0e21ead4ea980724c41456b7ce26428cc24
Olivia <olitoxqui@gmail.com>
2023-05-19 13:19:14 -0600

    Se agregó la vista del carrito y también el controller de productos

---
+++ Mis proyectos/ecommerce-Curso/spring-ecommerce/spring-ecommerce/src/main/java/com/curso/ecommerce/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.curso.ecommerce.service;
+
+import java.util.Optional;
+
+import com.curso.ecommerce.entity.Producto;
+
+public interface ProductService {
+
+	public Producto save(Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+	
+}
---
+++ Mis proyectos/ecommerce-Curso/spring-ecommerce/spring-ecommerce/src/main/java/com/curso/ecommerce/service/ProductoServiceImp.java
@@ -0,0 +1,37 @@
+package com.curso.ecommerce.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.curso.ecommerce.entity.Producto;
+import com.curso.ecommerce.repository.ProductRepository;
+
+public class ProductoServiceImp implements ProductService{
+
+	@Autowired
+	ProductRepository productRepository;
+	 
+	@Override
+	public Producto save(Producto producto) {
+		return productRepository.save(producto);
+	}
+
+	@Override
+	public Optional<Producto> get(Integer id) {
+		return productRepository.findById(id);
+	}
+
+	@Override
+	public void update(Producto producto) {
+		productRepository.save(producto);
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		productRepository.deleteById(id);
+		
+	}
+
+}
