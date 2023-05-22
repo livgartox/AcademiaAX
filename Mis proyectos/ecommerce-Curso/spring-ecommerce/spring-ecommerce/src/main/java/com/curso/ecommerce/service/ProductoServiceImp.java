25e04fcaae249a8725b71946c8bc5209a3ad88b8
Olivia <olitoxqui@gmail.com>
2023-05-22 12:49:32 -0600

    funcionalidad agregar productos

--- Mis proyectos/ecommerce-Curso/spring-ecommerce/spring-ecommerce/src/main/java/com/curso/ecommerce/service/ProductoServiceImp.java
+++ Mis proyectos/ecommerce-Curso/spring-ecommerce/spring-ecommerce/src/main/java/com/curso/ecommerce/service/ProductoServiceImp.java
@@ -3,10 +3,12 @@ package com.curso.ecommerce.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.curso.ecommerce.entity.Producto;
 import com.curso.ecommerce.repository.ProductRepository;
 
+@Service
 public class ProductoServiceImp implements ProductService{
 
 	@Autowired
